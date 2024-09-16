02cd7e80a4171abb77ee1a7fc252057d98e0676f
Ratel Mohamed Wael <129957067+RatelMedWael@users.noreply.github.com>
2024-09-16 13:04:58 +0100

    Revert "newapp"

--- my-app/src/utils/imageGenerator.js
+++
@@ -1,8 +0,0 @@
-export const generateImageLinks = (count) => {
-  const imageLinks = [];
-  for (let i = 0; i < count; i++) {
-    const randomHeight = Math.floor(Math.random() * (900 - 300 + 1)) + 300;
-    imageLinks.push(`https://picsum.photos/${randomHeight}`);
-  }
-  return imageLinks;
-};
